49afc930fab997c10f7232a49e305ada20fa1604
Brynna <brynna.mccarter@gmail.com>
2017-02-24 10:13:25 -0800

    add homepage, basic silex routing for Stylist

--- app/app.php
+++ app/app.php
@@ -3,17 +3,32 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
 
+    $app = new Silex\Application();
+
     $server = 'mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), ["twig.path" => __DIR__."/../views"]);
 
     $app->get('/', function() use($app) {
-        $result = 'hello';
-        return $app["twig"]->render("root.html.twig", ['result' => $result]);
+
+        return $app["twig"]->render("homepage.html.twig", ['stylists' => Stylist::getAll()]);
+    });
+
+    $app->post('add_stylist', function() use($app) {
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $new_stylist = new Stylist($name, $description);
+        $new_stylist->save();
+        return $app->redirect('/');
+    });
+
+    $app->get('/stylists/{id}', function($id) use($app)  {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', ['stylist' => $stylist]);
     });
 
     return $app;
